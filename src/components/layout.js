3c3a81ffb5df2d9e9c7c4617fb0bd69cd89958c7
yangzhou <982505136@qq.com>
2018-10-11 01:36:26 +0800

    layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Layout } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
+const renderHeader = () => {
+  return (
+    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+    </Header>
+  );
+};
+
+const renderContent = () => {
+  return (
+    <Content style={{ padding: '0 50px', marginTop: 64 }}>
+    </Content>
+  );
+};
+
+const renderFooter = () => {
+  return (
+    <Footer style={{ textAlign: 'center' }}>
+      Yang Zhou ©2018
+    </Footer>
+  );
+};
 
 
-const Layout = ({ match }) => (
-  <div>
-    <Button type="primary">Primary</Button>
-  </div>
+const Layouts =  ({ match }) => (
+  <Layout>
+    { renderHeader() }
+    { renderContent() }
+    { renderFooter() }
+  </Layout>
 );
 
-export default Layout;
+export default Layouts;
