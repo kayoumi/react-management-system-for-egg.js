9e2fd453415fe2195edbb3da012c754153b69224
yangzhou <982505136@qq.com>
2018-10-31 00:21:57 +0800

    测试在react中使用yml格式的数据

--- src/components/main-tab/operation.js
+++ src/components/main-tab/operation.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Tabs } from 'antd';
+import yaml from 'yamljs';
 import './index.less';
+import yamlData from '../../data/data.yml';
+import yamlData2 from '../../data/data.yaml.js';
 
-const TabOperation = () => (
-    <div>
-        这里写：用户操作
-    </div>
-);
+const parsedYamlData = yaml.load(yamlData);
+const parsedYamlData2 = yaml.parse(yamlData2);
+
+const TabOperation = () => {
+    console.log(yamlData);
+    console.log(yamlData2);
+    console.log(parsedYamlData);
+    console.log(parsedYamlData2);
+    return (
+        <div>
+            这里写：用户操作
+            { parsedYamlData.contact[1].icon }
+            { parsedYamlData2.contact[0].icon }
+        </div>
+    );
+}
 
 export default TabOperation;
---
+++ src/data/data.yaml.js
@@ -0,0 +1,24 @@
+const yamlData = `
+---
+update_time: '2018.10.30'
+name: 杨舟
+gender: 男
+nickname: Candybullet
+meta_content: 程序员是无所不能的。
+contact: # 联系方式
+  -
+    icon: fa-envelope
+    name: Email
+    value: zhangruiwu32@gmail.com
+  -
+    icon: fa-qq
+    name: QQ
+    value: '1335497539'
+  -
+    icon: fa-phone-square
+    name: PhoneNumber
+    value: '18729316081'
+---
+`;
+
+export default yamlData;
